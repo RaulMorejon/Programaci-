c1f277c20c07453e304af8afa17337c08cc4d863
raul_insebre <raulmorejon@iesebre.com>
2024-10-15 11:32:43 +0200

    Dimarts15OCT

---
+++ src/main/java/Divisors.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Divisors {
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        int casos = entrada.nextInt();
+        for (int i = 0; i < casos ; i++) {
+            int numero = entrada.nextInt();
+            for (int j = 1; j <= numero ; j++) {
+                if(numero%j==0) System.out.print(j+" ");
+            }
+            System.out.println(" ");
+        }
+
+    }
+}
